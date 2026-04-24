e00821f6cfdc1a5497da40383856a08841b485f1
yassine grairi <grairiiyassine@gmail.com>
2026-04-24 22:15:00 +0100

    hero section for homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,53 +1,10 @@
+import ChooseGameSettings from "./homeCompononts/ChooseGameSettings";
+import Hero from "./homeCompononts/Hero";
+
 export default function Home({ gameSettings, setGameSettings }) {
   return (
-    <div className="h-dvh  bg-[url(/images/homeBg.png)] bg-cover">
-      <form
-        action=""
-        onSubmit={(e) => {
-          e.preventDefault();
-          const fd = new FormData(e.currentTarget);
-          setGameSettings({
-            ...gameSettings,
-            imgType: fd.get("selectMode"),
-            diff: fd.get("selectDiff"),
-          });
-        }}
-      >
-        <h2>chose img type:</h2>
-        <input
-          value="characters"
-          type="radio"
-          id="character"
-          name="selectMode"
-          defaultChecked
-        />
-        <label htmlFor="character">character</label>
-        <input value="devilFruits" type="radio" id="df" name="selectMode" />
-        <label htmlFor="df">devil fruit</label>
-
-        <h2>chose diff:</h2>
-        <input
-          defaultChecked
-          value="easy"
-          type="radio"
-          id="easy"
-          name="selectDiff"
-        />
-        <label htmlFor="easy">easy</label>
-        <input value="medium" type="radio" id="medium" name="selectDiff" />
-        <label htmlFor="medium">medium</label>
-        <input value="hard" type="radio" id="hard" name="selectDiff" />
-        <label htmlFor="hard">hard</label>
-        <br />
-
-        <button
-          className="bg-amber-400 p-2 rounded-xl hover:bg-amber-200"
-          type="submit"
-        >
-          {" "}
-          Start game
-        </button>
-      </form>
-    </div>
+    <main className="h-dvh  bg-[url(/images/homeBg.png)] bg-cover flex flex-col items-center">
+      <Hero />
+    </main>
   );
 }
---
+++ src/components/homeCompononts/Hero.jsx
@@ -0,0 +1,80 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { CustomEase } from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
+const Hero = () => {
+  const hero = useRef();
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+      tl.from(".title", {
+        y: -100,
+        ease: "back.out(2)",
+      });
+
+      tl.from("#badge", {
+        opacity: 0,
+        y: 20,
+        delay: -0.2,
+        duration: 1,
+        ease: "elastic.out(1)",
+      });
+
+      tl.to(
+        "#hat",
+        {
+          y: 145,
+          x: -20,
+          scale: 1.7,
+          rotate: -5,
+          ease: "expoScale(0.5,7,power1.inOut)",
+          duration: 0.4,
+        },
+        "-=0.5",
+      );
+    },
+    { scope: hero },
+  );
+
+  return (
+    <section
+      ref={hero}
+      className="p-5  contrast-120 brightness-95 overflow-hidden h-70 relative w-100 "
+    >
+      <img
+        src="/images/One-Piece-Logo.png"
+        alt=""
+        className="title w-42 absolute z-10 left-28 top-4.5"
+      />
+      <img
+        src="/images/board.png"
+        alt=""
+        className="title -rotate-2 pt-11 brightness-80 "
+      />
+
+      <img
+        className=" title absolute w-72 top-21 left-16.5 -rotate-2"
+        src="/images/title.png"
+        alt=""
+      />
+
+      <img
+        id="badge"
+        className="w-77 absolute left-13 top-40"
+        src="/images/badge.png"
+        alt=""
+      />
+
+      <img
+        id="hat"
+        src="/images/hat.png"
+        alt=""
+        className="absolute w-14 -top-18 right-3 z-1 rotate-90"
+        style={{ filter: "drop-shadow(-10px 10px 5px rgba(0, 0, 0, 0.7))" }}
+      />
+    </section>
+  );
+};
+
+export default Hero;
